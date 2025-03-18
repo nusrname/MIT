05f39e9a5aa3a9aa3f14d5762205dba12e8d4d4c
newUsername <zhelezniyche@yandex.ru>
2025-03-18 19:56:53 +0300

    added function fib() to output the n-th Fibonacci number

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int fib(int n) 
+{
+	if (n <= 2) return 1;
+	return fib(n - 1) + fib(n - 2);
+}
+
 int main()
 {
+	int n;
+	cout << "Input count: ";
+	cin >> n;
+	cout << fib(n) << endl;
 	cout << "Hello world!" << endl;
 }
